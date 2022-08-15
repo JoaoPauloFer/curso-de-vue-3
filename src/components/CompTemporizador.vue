626507117f88e8d8adc3d34338c8d2f1f6b96faf
João Paulo <joaopaulo.data@gmail.com>
2022-08-15 12:04:45 -0300

    docs: 📑  Criando componente para o botão

--- src/components/CompTemporizador.vue
+++ src/components/CompTemporizador.vue
@@ -1,18 +1,8 @@
 <template>
 <div class="is-flex is-align-items-center is-justify-content-space-between">
     <Cronometro :tempoEmSegundos="tempoEmSegundos" />
-        <button class="button" @click="iniciar" :disabled="cronometroRodando">
-            <span class="icon">
-                <i class="fas fa-play"></i>
-            </span>
-            <span>play</span>
-        </button>
-        <button class="button" @click="finalizar" :disabled="!cronometroRodando">
-            <span class="icon">
-                <i class="fas    fa-stop"></i>
-            </span>
-            <span>stop</span>
-        </button>
+    <Botao @clicado="iniciar" icone="fas fa-play" texto="play" :desabilitado="cronometroRodando" />
+    <Botao @clicado="finalizar" icone="fas fa-stop" texto="stop" :desabilitado="!cronometroRodando" />
 </div>
 </template>
 
@@ -20,12 +10,14 @@
 
 import { defineComponent } from 'vue'
 import Cronometro from './CompCronometro.vue'
+import Botao from './CompBotao.vue'
 
 export default defineComponent ({
     name: "CompTemporizador",
     emits: ['aoTemporizadorFinalizado'],
     components: {
-        Cronometro
+        Cronometro,
+        Botao
     },
     data() {
         return {
